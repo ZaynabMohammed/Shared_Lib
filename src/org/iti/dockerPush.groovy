29aae1d0ac1994276374abf1de91abf98ad861e6
Zaynab <68663353+ZaynabMohammed@users.noreply.github.com>
2025-08-16 03:58:49 +0300

    Update dockerPush.groovy

--- src/org/iti/dockerPush.groovy
+++ src/org/iti/dockerPush.groovy
@@ -1,4 +1,5 @@
-def call(String imageName, String buildNumber, String dockerUser, String dockerPass) {
+package org.iti;
+def dockerPush(String imageName, String buildNumber, String dockerUser, String dockerPass) {
     stage("Docker Push") {
         sh "echo ${dockerPass} | docker login -u ${dockerUser} --password-stdin"
         sh "docker push ${imageName}:v${buildNumber}"
